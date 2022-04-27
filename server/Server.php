94fa2bc92a078a0a7c983528518e04adea9027f9
Eric Tao <twqzy323@163.com>
2022-04-28 00:22:39 +0800

    move Server.php to /server, add bin/phpserver

--- server/Server.php
+++ server/Server.php
@@ -1,22 +1,40 @@
 <?php
 
-require_once __DIR__.'/worker/TextWorker.php';
+require_once __DIR__.'/../worker/TextWorker.php';
 
+/**
+ * Server，用于管理workers.
+ */
 Class Server
 {
     public static function GetWorker(string $name, array $workerCfg, array $servCfg)
     {
         $workerType = $workerCfg['worker_class'];
         switch ($workerType) {
-            case 'ZKTextWorker':
+            case 'TextWorker':
                 return new TextWorker($workerCfg, $servCfg);
             default:
                 return null;
         }
     }
 
-    public static function Start($appSettings)
+    /**
+     * 获取应用设置.
+     * 
+     * @return array
+     */
+    public static function AppSettings()
     {
+        $appSettings = require __DIR__.'/../config/main.php';
+        return $appSettings;
+    }
+
+    /**
+     * 启动服务.
+     */
+    public static function Start()
+    {
+        $appSettings = self::AppSettings();
         if (empty($appSettings['workers'])) {
             echo 'empty workers', PHP_EOL;
             return;
@@ -27,14 +45,22 @@ Class Server
         ];
         foreach ($appSettings['workers'] as $name => $config) {
             $worker = self::GetWorker($name, $config, $servCfg);
+            var_dump($worker);
             if (empty($worker)) {
                 continue;
             }
             $worker->Start();
         }
     }
+
+    /**
+     * 停止服务.
+     */
+    public static function Stop()
+    {
+    }
 }
 
 ///////////////////// start server /////////////////
-$settings = include __DIR__.'/config/main.php';
-Server::Start($settings);
+// $settings = include __DIR__.'/config/main.php';
+// Server::Start($settings);
